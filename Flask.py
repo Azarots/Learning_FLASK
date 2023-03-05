27acf5527e652fe596b92f78370d797905251987
Azarots <Azarots159@gmail.com>
2023-03-05 14:49:20 +0100

    Added BootStrap

--- Flask.py
+++ Flask.py
@@ -1,22 +1,14 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return "Hello! This is the main page <h1>HELLO<h1>"
+    return render_template("home_page.html")
 
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
-
+@app.route("/test")
+def test():
+    return render_template("New.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
